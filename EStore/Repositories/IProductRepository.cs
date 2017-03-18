9954a3b84efac9977632a58b94f47911a521a3f3
Thehink <benja280@gmail.com>
2017-03-18 21:00:20 +0100

    added repository for products

---
+++ EStore/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using EStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Repositories
+{
+    public interface IProductRepository
+    {
+        void Add(Product item);
+        IEnumerable<Product> GetAll();
+        Product Find(int id);
+        void Remove(int id);
+        void Update(Product item);
+    }
+}
---
+++ EStore/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using EStore.Data;
+using EStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Repositories
+{
+    public class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public void Add(Product product)
+        {
+
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return this._context.Products.ToList();
+        }
+
+        public IEnumerable<Product> Filter(string str, int categoryId, decimal maxPrice = 0, decimal minPrice = 0)
+        {
+            return this._context.Products.ToList();
+        }
+
+        public Product Find(int id)
+        {
+            return new Product();
+        }
+
+        public void Remove(int id)
+        {
+
+        }
+
+        public void Update(Product product)
+        {
+
+        }
+    }
+}
