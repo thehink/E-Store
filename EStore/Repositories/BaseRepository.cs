4387cd04cb441c0cf88c285494afa24b2ae8276d
Thehink <benja280@gmail.com>
2017-03-18 21:00:20 +0100

    added base repository

---
+++ EStore/Repositories/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using EStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Repositories
+{
+    public abstract class BaseRepository
+    {
+
+        protected ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+    }
+}
