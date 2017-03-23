8ff56a20e359340ec9675cfafca732f7f016e11a
Thehink <benja280@gmail.com>
2017-03-23 22:36:37 +0100

    added empty admin controller

---
+++ EStore/Controllers/Admin/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Controllers.Admin
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController
+    {
+    }
+}
